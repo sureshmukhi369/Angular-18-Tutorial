475161f6082ad2596366a46abe0d0b358e1df60c
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-02-28 12:31:58 +0530

    Styled add-employee and employee-list components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent], // ✅ Make sure both components are imported
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Angular-18-Tutorial';
--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { EmployeeListComponent } from "../employee-list/employee-list.component";
 
 @Component({
   selector: 'app-add-employee',
   standalone: true,
-  imports: [],
+  imports: [EmployeeListComponent],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.css'
 })
